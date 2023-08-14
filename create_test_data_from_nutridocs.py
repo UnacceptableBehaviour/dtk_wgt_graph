bd71c81fce52a9605c2ca01b17fd20986cd18dea
UnacceptableBehaviour <fernandez.simon@gmail.com>
2023-08-14 22:38:34 +0100

    Script to Generate data for progress graph.

---
+++ create_test_data_from_nutridocs.py
@@ -0,0 +1,128 @@
+#! /usr/bin/env python
+
+import sys
+from pathlib import Path
+from pprint import pprint
+import re
+import json # JSONDecodeError
+from datetime import datetime
+from timestamping import nix_time_ms, hr_readable_date_from_nix #, hr_readable_w_nix_ms_from_nix
+import shutil
+# RTF conversion to text
+from striprtf.striprtf import rtf_to_text
+
+opt_dict = {
+    'verbose_mode':     False,
+}
+
+if '-v' in sys.argv:
+    opt_dict['verbose_mode'] = True
+
+
+help_string = '''
+- - - options - - - 
+-v          verbose_mode
+
+-h          This help
+'''
+
+if ('-h' in sys.argv) or ('--h' in sys.argv) or ('-help' in sys.argv) or ('--help' in sys.argv):
+    print(help_string)
+    sys.exit(0)
+
+
+
+
+DATA_FILE_JSON = Path('./docs/static/data/dtk_data.json')
+DATA_FILE_JS = Path('./docs/static/js_modules/content/dtk_data.js')
+
+# with open(DATA_FILE_JSON, 'r') as f:
+#     record = json.load(f)
+
+# record_to_json_file = json.dumps(record)
+# with open(DATA_FILE_JSON, 'w') as f:
+#     f.write(record_to_json_file)
+
+def get_nutridoc_list_rtf(base_dir):    
+
+    #y971_NUTRITEST_recipes_20191123-06.rtf
+
+    file_LUT = {}
+    nutri_doc_ref =''
+
+    print(f'\nNutridocs Found:{base_dir}')
+    for file_loc in base_dir.rglob('*_NUTRITEST_recipes_*.rtf'):        
+        nutri_doc_ref = re.search(r'(y\d{3})_', file_loc.stem).group(1)
+        file_LUT[nutri_doc_ref] = file_loc
+
+        if opt_dict['verbose_mode']:
+            print(f"{nutri_doc_ref} : {file_loc.name} - {file_loc} ")
+        else:
+            print(re.search(r'(y\d{3})_', file_loc.stem).group(1), end=' ')
+    
+    print('\n')
+
+    return file_LUT
+
+nutridocs_base_dir = Path('/Users/simon/Desktop/supperclub/foodlab/_MENUS/_courses_components/')
+file_LUT = get_nutridoc_list_rtf(nutridocs_base_dir)
+
+
+def get_text_content_of_file(rtf_filepath):
+
+    with open(rtf_filepath,'r') as f:
+        rtf = f.read()
+
+    return rtf_to_text(rtf) 
+
+record = {}
+c = 0
+for k,f in file_LUT.items():
+    txt = get_text_content_of_file(f)
+    print(f"\n\n{k} - {f}")
+    
+    patLine = r"^-*? (for the.*?calories.*?)$"
+    # from m02     yr             mnth                  dayL                  dayD      wgt
+    pat02 =  r"(\d\d\d\d).*?month (\d+)\s+(day|mon|tue|wed|thu|fri|sat|sun)\s+(\d+).*?(\d+\.\d+)kg"
+    pat03 =  r"fat.*?(\d\d\.\d).*?h2o.*?(\d\d\.\d)"
+    patZ = r"(\d+\.\d+kg)(?:.*?fat_pc.*?(\d+\.\d+))?.*?(?:H2O_pc.*?(\d+\.\d+))?"
+
+    for line in txt.splitlines():
+        m1 = re.match(patLine, line, re.I)
+        if m1:
+            if 'sun mon tue' in line: continue
+            yr,mnth,dayD = 1970,1,1
+            lipids, h2o = None, None
+
+            print(f"M(1): {m1.group(1)}")
+            
+            m02 = re.search(pat02, line, re.I)
+            if m02:
+                yr, mnth, dayL, dayD, wgt = m02.groups()
+            
+            m03 = re.search(pat03, line, re.I)
+            if m03:
+                lipids, h2o = m03.groups()
+            
+            
+            nix_time = nix_time_ms(datetime(int(yr),int(mnth),int(dayD)))
+            hr_date = hr_readable_date_from_nix(nix_time)   # pad out single digits
+            record[nix_time] = {'dtk_pc_fat': lipids,
+                                'dtk_pc_h2o': h2o,
+                                'dtk_rcp': {'dt_date': nix_time,
+                                            'dt_date_readable': hr_date,
+                                            'dt_day': dayL},
+                                'dtk_user_info': {'UUID': 'x-x-x-x-xxx',
+                                                'name': 'AGCT'},
+                                'dtk_weight': wgt}
+
+#pprint(record)
+
+record_to_json = json.dumps(record)
+with open(DATA_FILE_JSON, 'w') as f:
+    f.write(record_to_json)
+
+record_js = f"export let dtk_test_record = {record_to_json};"
+
+with open(DATA_FILE_JS, 'w') as f:
+    f.write(record_js)
